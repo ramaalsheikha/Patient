f64572998e8d8a36600795576f2e2522aa24440c
rama <alsheikharama@gmail.com>
2024-03-01 04:43:31 +0300

    Creat Repository Modul class

---
+++ data/src/main/java/com/example/patient/data/di/RepoModule.kt
@@ -0,0 +1,23 @@
+package com.example.patient.data.di
+
+import com.example.patient.data.datasource.PatientDataSource
+import com.example.patient.data.repository.PatientRepositoryImpl
+import com.example.patient.domain.repo.PatientRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepoModule {
+
+    @Provides
+    @Singleton
+    fun provideRepo(patientDataSource: PatientDataSource):PatientRepository{
+
+        return PatientRepositoryImpl(patientDataSource)
+    }
+}
--- data/src/main/java/com/example/patient/data/repository/PatientRepositoryImpl.kt
+++ data/src/main/java/com/example/patient/data/repository/PatientRepositoryImpl.kt
@@ -2,11 +2,12 @@ package com.example.patient.data.repository
 
 import com.example.patient.data.datasource.PatientDataSource
 import com.example.patient.domain.model.patiens.PatientRemoteModel
+import com.example.patient.domain.repo.PatientRepository
 import javax.inject.Inject
 
-class PatientRepositoryImpl @Inject constructor(private val patientDataSource: PatientDataSource) {
+class PatientRepositoryImpl  @Inject constructor(private val patientDataSource: PatientDataSource) :PatientRepository {
 
-    suspend fun getPatients(): List<PatientRemoteModel> {
+   override suspend fun getPatients(): List<PatientRemoteModel> {
         return patientDataSource.getPatients().data.sortedBy { it.name }
     }
 }
